76d05e87c60c911d3b5fb5904afdf25b9cf379ad
srush <srush.research@gmail.com>
2019-07-16 18:29:14 +0000

    .

--- cv/make.sh
+++ cv/make.sh
@@ -1,5 +1,5 @@
 cat ../_data/papers.yaml ../_data/code.yaml > /tmp/all.yaml
-jinja2 cv.tex /tmp/all.yaml --format yaml > /tmp/cv.tex
-pdflatex /tmp/cv.tex
-jinja2 cv_short.tex /tmp/all.yaml --format yaml > /tmp/cv_short.tex
-pdflatex /tmp/cv_short.tex
+jinja2 cv.tex /tmp/all.yaml --format yaml > cv.comp.tex
+pdflatex cv.tex
+jinja2 cv_short.tex /tmp/all.yaml --format yaml > cv_short.comp.tex
+pdflatex cv_short.tex
