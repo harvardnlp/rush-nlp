0c05e6903f6cff9990963ac24396ddcb6b8038fe
Sasha <arush@g.harvard.edu>
2019-12-18 18:33:14 -0500

    .

--- cv/make.sh
+++ cv/make.sh
@@ -1,5 +1,5 @@
 cat ../_data/papers.yaml ../_data/code.yaml > /tmp/all.yaml
 jinja2 cv.tex /tmp/all.yaml --format yaml > cv.comp.tex
-pdflatex cv.tex
+pdflatex cv.comp.tex
 jinja2 cv_short.tex /tmp/all.yaml --format yaml > cv_short.comp.tex
-pdflatex cv_short.tex
+pdflatex cv_short.comp.tex
