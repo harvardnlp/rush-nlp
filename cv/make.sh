16664145ec07ba3df7e6baa9e91b54b20022999f
Sasha <srush.research@gmail.com>
2024-11-18 11:11:26 -0500

    update

--- cv/make.sh
+++ cv/make.sh
@@ -1,7 +1,7 @@
 cat ../_data/papers.yaml ../_data/extra.yaml ../_data/code.yaml ../_data/projects.yaml > /tmp/all.yaml
-jinja2 cv.tex /tmp/all.yaml --format yaml > cv.comp.tex
+uvx --with jinja2-cli --with pyyaml jinja2 cv.tex /tmp/all.yaml --format yaml > cv.comp.tex
 pdflatex cv.comp.tex
-jinja2 cv_short.tex /tmp/all.yaml --format yaml > cv_short.comp.tex
+uvx --with jinja2-cli --with pyyaml jinja2 cv_short.tex /tmp/all.yaml --format yaml > cv_short.comp.tex
 pdflatex cv_short.comp.tex
-jinja2 cv_tenure.tex /tmp/all.yaml --format yaml > cv_tenure.comp.tex
+uvx --with jinja2-cli --with pyyaml jinja2 cv_tenure.tex /tmp/all.yaml --format yaml > cv_tenure.comp.tex
 pdflatex cv_tenure.comp.tex
